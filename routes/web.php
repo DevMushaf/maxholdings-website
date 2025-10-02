f7d4d8b4e130fcb51ddc7fb2bf1af6724e6343ce
AmriShanab <amrishanab@gmail.com>
2025-10-02 13:20:51 +0530

    Push For Server

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ Route::get('/services', [ServicesController::class, 'index'])->name('services');
 Route::get('/blogs', [BlogsController::class, 'index'])->name('blogs');
 Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blog.show');
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+
+// Sample Routes for other pages
